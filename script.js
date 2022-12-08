7072f20a5f24ea679982ae41437ece5fa3890380
tiagoorlamunder <tiago.orlamunder@gmail.com>
2022-12-07 22:03:11 -0300

    activated operations

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-function insert(digit){
-    var digit = document.getElementById(digit).innerText
-    document.getElementById('screen').innerText += digit
+function isNumber(digit) {
+    if (digit == 'one' || digit == 'two' || digit == 'three' || digit == 'four' || digit == 'five' || digit == 'six' || digit == 'seven' || digit == 'eigth' || digit == 'nine') {
+        return true
+    }
+    return false
 }
 
-function clean(){
+function isOperator(digit) {
+    if (digit == 'multiplication' || digit == 'division' || digit == 'addition' || digit == 'subtraction') {
+        return true
+    }
+    return false
+}
+
+function isDot(digit) {
+    if (digit == 'dot') {
+        return true
+    }
+    return false
+}
+
+function clean() {
     document.getElementById('screen').innerText = ''
 }
 
+function insert(digit) {
+    var digits = document.getElementById('screen').innerText
+    var digit = document.getElementById(digit).innerText
+    if (digits.length < 18) {
+        document.getElementById('screen').innerText += digit
+    }
+}
+
 function calculate() {
-    alert('Ainda não é possível calcular :D')
-    clean()
+    var digits = document.getElementById('screen').innerText
+    document.getElementById('screen').innerText = eval(digits)
 }
