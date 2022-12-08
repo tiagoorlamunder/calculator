375b9e6b2953b178b156c6d070f21ac44bf1749f
tiagoorlamunder <tiago.orlamunder@gmail.com>
2022-12-08 01:31:54 -0300

    bug fixed in dots input

--- script.js
+++ script.js
@@ -26,7 +26,19 @@ function isDot(digit) {
     return false
 }
 
+function isDot(digit) {
+    if (digit == '.') {
+        return true
+    }
+    return false
+}
 
+function isEmpty(digit) {
+    if (digit == '') {
+        return true
+    }
+    return false
+}
 
 function clean() {
     document.getElementById('screen').innerText = ''
@@ -34,12 +46,18 @@ function clean() {
 
 function insert(digit) {
     var digits = document.getElementById('screen').innerText
-    if (digits.length < 18 && !(digits == '' && (isZero(digit) || isOperator(digit) || isDot(digit)))) {
-        var lastDigit = digits.slice(-1)
-        if (isOperator(lastDigit) && isOperator(digit)) {
-            document.getElementById('screen').innerText = digits.slice(0, -1) + digit
-        } else {
-            document.getElementById('screen').innerText += digit
+    var lastDigit = digits.slice(-1)
+    if (!(isEmpty(digits) && (isZero(digit) || isOperator(digit) || isDot(digit)))) {
+        if (!((isDot(lastDigit) && (isOperator(digit))) || (isDot(digit) &&  digits.includes('.')))) {
+            if (!(isOperator(lastDigit) && isDot(digit))) {
+                if (digits.length < 18) {
+                    if (isOperator(lastDigit) && isOperator(digit)) {
+                        document.getElementById('screen').innerText = digits.slice(0, -1) + digit
+                    } else {
+                        document.getElementById('screen').innerText += digit
+                    }
+                }
+            }
         }
     }
 }
