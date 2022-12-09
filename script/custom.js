752a3e5502b6da27309da02576b9d56baea58194
tiagoorlamunder <tiago.orlamunder@gmail.com>
2022-12-09 18:15:20 -0300

    new features added

--- script/custom.js
+++ script/custom.js
@@ -1,13 +1,45 @@
-function insert(digit) {
+function calculate() {
     var digits = document.getElementById('screen').innerText
-    var lastNumber = returnsLastNumber(digits)
+    if (digits != '')
+    document.getElementById('screen').innerText = eval(digits)
+}
+
+function insert(digit) {
+    var status = returnStatus()
+    var digits = document.getElementById('screen').innerText
+    var lastNumber = returnLastNumber(digits)
     var lastDigit = digits.slice(-1)
     var lastButOneDigit = digits.slice(-2, -1)
-    if (digits.length >= 22) {
+    if (digit == 'on/off') {
+        var status = document.getElementById('on/off').innerText
+        if (status == 'O') {
+            document.getElementById('on/off').innerText = '|'
+            document.getElementById('screen').innerText = ''
+        } else if (status == '|') {
+            document.getElementById('on/off').innerText = 'O'
+            document.getElementById('screen').innerText = 0
+        }
+    } else if (status == 'off') {
+        console.log('This digit cannot be typed now...')
+    } else if (digit == 'C') {
+        document.getElementById('screen').innerText = '0'
+    } else if (digit == 'CE') {
+        if (digits == lastNumber) {
+            document.getElementById('screen').innerText = '0'
+        } else {
+            document.getElementById('screen').innerText = removeLastNumber(digits)
+        }
+    } else if (hasLyrics(digits)) {
+        console.log('This digit cannot be typed now...')
+    } else if (digit == '<') {
+        if (digits.length == 1) {
+            document.getElementById('screen').innerText = '0'
+        } else {
+            document.getElementById('screen').innerText = digits.slice(0, -1)
+        }
+    } else if (digits.length >= 22) {
         console.log('This digit cannot be typed now...')
     } else if (digits.length >= 21 && isOperator(digit)) {
-        console.log('This digit cannot be typed now...')
-    } else if (hasLyrics(digits)) {
         console.log('This digit cannot be typed now...')
     } else if (isEmpty(digits) && isDot(digit)) {
         console.log('This digit cannot be typed now...')
@@ -36,9 +68,4 @@ function insert(digit) {
             document.getElementById('screen').innerText += digit
         }
     }
-}
-
-function calculate() {
-    var digits = document.getElementById('screen').innerText
-    document.getElementById('screen').innerText = eval(digits)
 }
--- script/utils.js
+++ script/utils.js
@@ -1,4 +1,4 @@
-function returnsLastNumber(digits) {
+function returnLastNumber(digits) {
     var lastNumber = ''
     for (d = digits.length - 1; d >= 0; d--) {
         if (isOperator(digits[d])) {
@@ -7,4 +7,29 @@ function returnsLastNumber(digits) {
         lastNumber = digits[d] + lastNumber
     }
     return lastNumber
+}
+
+function returnStatus() {
+    var button = document.getElementById('on/off').innerText
+    if (button == 'O') {
+        return 'on'
+    }
+    else if (button == '|') {
+        return 'off'
+    }
+}
+
+function removeLastNumber(digits){
+    rest = digits
+    for (d = digits.length - 1; d >= 0; d--) {
+        rest = rest.slice(0, -1)
+        digit = digits[d]
+        console.log('digits: ' + digits)
+        console.log('digit: ' + digit)
+        console.log('d: ' + d)
+      if (isOperator(digit)){
+            return rest
+        }
+    }
+    return rest
 }
